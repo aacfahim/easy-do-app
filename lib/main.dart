38122e0c545e1db0d046b31b4717ea4013554ed1
aacfahim <aacfahim@gmail.com>
2024-01-25 17:17:50 +0600

    provider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:easy_do_app/screens/auth/sign_in.dart';
 import 'package:easy_do_app/screens/home.dart';
 import 'package:easy_do_app/screens/splash_screen.dart';
+import 'package:easy_do_app/services/auth_services.dart';
 import 'package:easy_do_app/widgets/custom_navbar.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -14,15 +18,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        primaryColor: const Color(0xFF8C88CD),
-        textTheme: GoogleFonts.manropeTextTheme(),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => AuthProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          primaryColor: const Color(0xFF8C88CD),
+          textTheme: GoogleFonts.manropeTextTheme(),
+          useMaterial3: true,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: SignIn(),
       ),
-      debugShowCheckedModeBanner: false,
-      home: CustomBottomNavigationBar(),
     );
   }
 }
