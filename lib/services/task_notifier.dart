86b0fe82274c3af7aa29d9021282301df0842116
aacfahim <aacfahim@gmail.com>
2024-01-28 00:40:04 +0600

    task notifier picked date added

--- lib/services/task_notifier.dart
+++ lib/services/task_notifier.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 
 class TaskNotifier extends ChangeNotifier {
   bool _shouldReload = false;
+  String? _pickedDate;
 
   bool get shouldReload => _shouldReload;
+  String? get pickedDate => _pickedDate;
 
   void setShouldReload(bool value) {
     _shouldReload = value;
+    notifyListeners();
+  }
+
+  void setPickedDate(String? date) {
+    _pickedDate = date;
     notifyListeners();
   }
 }
